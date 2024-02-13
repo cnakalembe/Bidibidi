619b758770c571bc6d7873b58e4b40b401833086
cnakalembe <cnakalem@umd.edu>
2024-02-13 15:50:12 -0500

    Update config.js

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ var config = {
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             //text for description is html- can rely on that for extra formatting
             location: {
-                center: [-122.418398, 37.759483],
+                center: [33.84700, -2.38360],
                 zoom: 8.5,
                 pitch: 60,
                 bearing: 0
